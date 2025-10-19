2ed9f84ba238dedf73ada6b9489e2f622e9a5850
寒鸽子 <2978778354@qq.com>
2025-10-19 20:59:59 +0800

    refactor(watch): 移除网络请求监听功能
    - 删除了 addEventListenerNetwork 方法及相关注释
    - 简化了导出对象，仅保留 url 变化监听
    - 清理了文件头部多余的空行

--- src/watch.js
+++ src/watch.js
@@ -1,5 +1,3 @@
-
-
 /**
  * 监听url变化
  * @param callback {function} 回调函数
@@ -15,42 +13,6 @@ const addEventListenerUrlChange = (callback,timeout = 1000) => {
     }, timeout);
 }
 
-
-/**
- * 监听网络请求
- * @param callback {function} 回调函数
- */
-const addEventListenerNetwork = (callback) => {
-    const performanceObserver = new PerformanceObserver(() => {
-        const entries = performance.getEntriesByType('resource');
-        const windowUrl = window.location.href;
-        const winTitle = document.title;
-        for (const entry of entries) {
-            const url = entry.name;
-            const initiatorType = entry.initiatorType;
-            if (initiatorType === "img" || initiatorType === "css" || initiatorType === "link" || initiatorType === "beacon") {
-                continue;
-            }
-            try {
-                callback(url, windowUrl, winTitle, initiatorType);
-            } catch (e) {
-                if (e.message === "stopPerformanceObserver") {
-                    performanceObserver.disconnect();
-                    console.log("已停止性能观察器对象实例", e)
-                    break;
-                }
-                throw e;
-            }
-        }
-        performance.clearResourceTimings();//清除资源时间
-    });
-    performanceObserver.observe({entryTypes: ['resource']});
-}
-
-
-
-
 export default {
-    addEventListenerUrlChange,
-    addEventListenerNetwork
+    addEventListenerUrlChange
 }
